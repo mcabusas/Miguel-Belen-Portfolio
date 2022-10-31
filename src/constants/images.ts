cb047fb604de5694f744ce06997465cfbb0849f9
Michael Cabusas <michaelcabusas@gmail.com>
2022-10-30 22:53:33 -0400

    Added font.

---
+++ src/constants/images.ts
@@ -0,0 +1,15 @@
+import image_1 from "../images/miggy_1.jpg";
+import image_2 from "../images/miggy_2.jpg";
+import image_3 from "../images/miggy_3.jpg";
+import image_4 from "../images/miggy_4.jpg";
+import image_5 from "../images/miggy_5.jpg";
+import image_6 from "../images/miggy_6.jpg";
+import image_7 from "../images/miggy_7.jpg";
+
+export default {
+  image_1,
+  image_2,
+  image_3,
+  image_4,
+  image_5,
+};
